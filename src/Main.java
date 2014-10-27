49d38befc55365fecc61970285b265312d238c59
jmbto <kagekyio@gmail.com>
2014-10-27 17:56:57 +0800

    Split, process and push instructions into instruction list.

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 public class Main {
+
+	static int hexStringToInt(String hs) {
+		return (int) Long.parseLong(hs, 16);
+	}
 
 	public static void main(String[] args) {
 
 		String file = args[1];
+		String[] split_line = null;
+		int[] int_split_line = {0, 0};
 		String line;
+
+		ArrayList<int[]> ins_list = new ArrayList<>();
+
 		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
 
-            for (int i = 0;  i < 10; i++) {
+            for (int i = 0;  i < 500; i++) {
                 line = br.readLine();
                 if (line != null)
-                    System.out.println(line);
+                    split_line = line.split(" ");
+                    int_split_line[0] = Integer.parseInt(split_line[0]);
+                    int_split_line[1] = hexStringToInt(split_line[1]);
+                    ins_list.add(int_split_line);
+                    System.out.println(split_line[0] + " " + split_line[1]);
+                    System.out.println(Integer.toHexString(int_split_line[1]));
 
 		    }
 		} catch (FileNotFoundException e) {
