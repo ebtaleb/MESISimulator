feb0a6fed2750a03dda6ccac3473985dae68ff8e
jmbto <kagekyio@gmail.com>
2014-10-27 19:38:30 +0800

    Some CLI parsing

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,50 @@ public class Main {
 		int[] int_split_line = null;
 		String line;
 
+		String protocol = args[0];
+		String file = args[1];
+		int no_processors = Integer.parseInt(args[2]);
+		int cache_size = Integer.parseInt(args[3]);
+		int associativity = Integer.parseInt(args[4]);
+		int block_size = Integer.parseInt(args[5]);
+
+		switch (associativity) {
+			case 1: break;
+			case 2: break;
+			case 4: break;
+			default: System.exit(0); break;
+		}
+
+		switch (no_processors) {
+			case 1: break;
+			case 2: break;
+			case 4: break;
+			case 8: break;
+			default: System.exit(0); break;
+		}
+
+		switch (cache_size) {
+			case 1024: break;
+			case 2048: break;
+			case 4096: break;
+			case 8192: break;
+			case 16384: break;
+			case 32768: break;
+			default: System.exit(0); break;
+		}
+
+		switch (block_size) {
+			case 1: break;
+			case 2: break;
+			case 4: break;
+			case 8: break;
+			case 16: break;
+			case 32: break;
+			case 64: break;
+			case 128: break;
+			default: System.exit(0); break;
+		}
+
 		ArrayList<int[]> ins_list = new ArrayList<>();
 
 		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
