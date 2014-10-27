bb9a465da836063179a2f3b90e39af9689130ff6
jmbto <kagekyio@gmail.com>
2014-10-27 19:36:40 +0800

    Fix instruction list push
    Was pushing the same array reference to the instruction list.

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String file = args[1];
 		String[] split_line = null;
-		int[] int_split_line = {0, 0};
+		int[] int_split_line = null;
 		String line;
 
 		ArrayList<int[]> ins_list = new ArrayList<>();
@@ -23,19 +22,22 @@ public class Main {
 
             for (int i = 0;  i < 500; i++) {
                 line = br.readLine();
-                if (line != null)
+                if (line != null) {
+                    int_split_line = new int[2];
                     split_line = line.split(" ");
                     int_split_line[0] = Integer.parseInt(split_line[0]);
                     int_split_line[1] = hexStringToInt(split_line[1]);
                     ins_list.add(int_split_line);
-                    System.out.println(split_line[0] + " " + split_line[1]);
-                    System.out.println(Integer.toHexString(int_split_line[1]));
-
-		    }
+                }
+            }
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+		for (int i = 0; i < 500; i++) {
+			System.out.println("[ " + ins_list.get(i)[0] + ", " + Integer.toHexString(ins_list.get(i)[1]) + " ]");
 		}
 
 	}
