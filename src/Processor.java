ad3d795fa075b345481cc3da02f29d3fbdae4fc3
jmbto <kagekyio@gmail.com>
2014-11-03 14:50:57 +0800

    no Memory is needed

--- src/Processor.java
+++ src/Processor.java
@@ -1,4 +1,17 @@
 
 public class Processor {
 
+	private int proc_id;
+	private Bus sharedBus;
+	private Cache proc_cache;
+	private int cycle_count;
+
+	public Processor(int proc_id, Bus b, int[] cache_settings) {
+		this.proc_id = proc_id;
+		this.sharedBus = b;
+		this.proc_cache = new Cache();
+		this.cycle_count = 0;
+	}
+
+
 }
