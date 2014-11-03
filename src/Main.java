96845dbefd46bfd30e99e5b1ceca3382def3adbb
jmbto <kagekyio@gmail.com>
2014-11-03 15:52:59 +0800

    Moves trace read to Processor class
    Now, each processor will read its own trace file line by line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -10,11 +7,7 @@ public class Main {
 		return (int) Long.parseLong(hs, 16);
 	}
 
-	public static void main(String[] args) {
-
-		String[] split_line = null;
-		int[] int_split_line = null;
-		String line;
+	public static void main(String[] args) throws FileNotFoundException {
 
 		String protocol = args[0];
 		String file = args[1];
@@ -60,28 +53,19 @@ public class Main {
 			default: System.exit(0); break;
 		}
 
-		ArrayList<int[]> ins_list = new ArrayList<>();
+		ArrayList<Processor> processors = new ArrayList<>();
+		Processor p = new Processor(0, null, null, file);
+		processors.add(p);
 
-		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+		while (true) {
+			try {
+				for (Processor cp: processors) {
+					cp.run();
+				}
 
-            for (int i = 0;  i < 500; i++) {
-                line = br.readLine();
-                if (line != null) {
-                    int_split_line = new int[2];
-                    split_line = line.split(" ");
-                    int_split_line[0] = Integer.parseInt(split_line[0]);
-                    int_split_line[1] = hexStringToInt(split_line[1]);
-                    ins_list.add(int_split_line);
-                }
-            }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		for (int i = 0; i < 500; i++) {
-			System.out.println("[ " + ins_list.get(i)[0] + ", " + Integer.toHexString(ins_list.get(i)[1]) + " ]");
+			} catch (Exception e) {
+				break;
+			}
 		}
 
 	}
--- src/Processor.java
+++ src/Processor.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Processor {
 
@@ -5,12 +9,42 @@ public class Processor {
 	private Bus sharedBus;
 	private Cache proc_cache;
 	private int cycle_count;
+	private BufferedReader ins_trace;
 
-	public Processor(int proc_id, Bus b, int[] cache_settings) {
+	static int hexStringToInt(String hs) {
+		return (int) Long.parseLong(hs, 16);
+	}
+
+	public Processor(int proc_id, Bus b, int[] cache_settings, String file) throws FileNotFoundException {
 		this.proc_id = proc_id;
 		this.sharedBus = b;
 		this.proc_cache = new Cache();
 		this.cycle_count = 0;
+		ins_trace = new BufferedReader(new FileReader(file));
+	}
+
+	public void run() throws Exception {
+		String line;
+		String[] split_line = null;
+		int[] int_split_line = null;
+
+        line = ins_trace.readLine();
+        if (line != null) {
+            int_split_line = new int[2];
+	        split_line = line.split(" ");
+	        int_split_line[0] = Integer.parseInt(split_line[0]);
+	        int_split_line[1] = hexStringToInt(split_line[1]);
+	        System.out.println("[ " + int_split_line[0] + ", " + Integer.toHexString(int_split_line[1]) + " ]");
+        } else {
+            throw new Exception();
+        }
+
+        if (cycle_count > 500) {
+            throw new Exception();
+        }
+
+        cycle_count++;
+
 	}
 
 
