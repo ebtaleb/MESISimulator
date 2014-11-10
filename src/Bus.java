04849fb910e862356e51532d21d856242d060477
Swetha <gswetha@hotmail.com>
2014-11-10 11:52:29 +0800

    Create bus and cache classes
    - cache class is still under development. Not yet sure how to handle
    load and store of data using tags, block number, set index, etc.

--- src/Bus.java
+++ src/Bus.java
@@ -1,4 +1,27 @@
 
 public class Bus {
+	private Cache[] caches;
+	private int countMessagesOnBus;
+	
+	public Bus(Cache[] caches){
+		this.caches = caches;
+		this.countMessagesOnBus = 0;
+	}
 
+	public Cache[] getCaches() {
+		return caches;
+	}
+
+	public void setCaches(Cache[] caches) {
+		this.caches = caches;
+	}
+
+	public int getCountMessagesOnBus() {
+		return countMessagesOnBus;
+	}
+
+	public void setCountMessagesOnBus(int countMessagesOnBus) {
+		this.countMessagesOnBus = countMessagesOnBus;
+	}
+	
 }
