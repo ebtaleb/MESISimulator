113fb25c3bdf53171eceb22a4c9b026904af698b
Swetha <gswetha@hotmail.com>
2014-11-11 13:11:56 +0800

    Cache changes in naming
    - changed set index to cache index to maintain consistency with notes
    - removed some comments
    - changed array list from sets to blocks (will be replaced later with a
    CacheLine array upon merge branch by Elias)

--- src/Cache.java
+++ src/Cache.java
@@ -7,12 +7,10 @@ public class Cache {
 	//#Sets = #Blocks / #Associativity
 	//Block number = mem address / blocksize
 	//Block offset (bits) = mem address mod blocksize
-	//Set index (bits) = log2(#Sets) --> set index and cache index are the same thing
+	//Cache index (bits) = log2(#Sets) --> cache index and set index are the same thing
 	//Tag bits in mem = length of address - block offset bits - set index bits  --> to differentiate memory blocks that map to the same cache block
 	//Mapping Function: Cache Index = (Block Number) modulo (Number of Cache Blocks)
 	//Cache hit: (Tag[index] = Tag[memory address]) AND (Valid[index] = TRUE)
-	
-	//do we calculate the no. bits for offset within block and the no. sets? then determine which bits represent the set index?
 	
 	private int cache_id;
 	public int cache_size;
@@ -26,8 +24,10 @@ public class Cache {
 	
 	//each set has n blocks for associativity = n
 	//each block has a valid bit, dirty bit and a tag
-	//each array index is the set index
-	private ArrayList<int[]> sets; 
+	//the array index of each block is its cache index. it should be in running order
+	//each block also has a cacheHit tracker which holds when it was hit (for LRU policy)
+	//eg. block 1 hit --> countCacheHit++; block.cacheHit = countCacheHit; in the case of cache miss, see which block.cacheHit is lowest and evict that block
+	private ArrayList<int[]> blocks; 
 	
 	public Cache(int cache_id, int cache_size, int associativity, int block_size, Bus bus, String protocol){
 		this.cache_id = cache_id;
@@ -39,7 +39,7 @@ public class Cache {
 		this.state = "INVALID";
 		this.countCacheMiss = 0;
 		this.countCacheHit = 0;
-		this.sets = new ArrayList<>();
+		this.blocks = new ArrayList<>();
 	}
 	
 	public int getNoBlocks(){
@@ -58,19 +58,19 @@ public class Cache {
 		return bits;
 	}
 	
-	private int getOffset(int address){ //is address an int?
+	private int getOffset(int address){ 
 		int bits = getOffsetBits();
 		return address % 10^bits;
 	}
 	
-	private int getSetIndexBits(){
+	private int getCacheIndexBits(){
 		//eg. 8 sets --> log8/log2 = 3 bits
 		int bits = (int) (Math.log(getNoSets())/Math.log(2));
 		return bits;
 	}
 	
-	private int getSetIndex(int address){
-		int bits = getSetIndexBits();
+	private int getCacheIndex(int address){
+		int bits = getCacheIndexBits();
 		String addressString = Integer.toString(address);
 		String result = addressString.substring(31-getOffsetBits()-bits+1,31-getOffsetBits());
 		return Integer.parseInt(result);
@@ -78,14 +78,14 @@ public class Cache {
 	
 	private int getTagBits(){
 		//assume 32 bit address
-		int bits = 32 - getOffsetBits() - getSetIndexBits();
+		int bits = 32 - getOffsetBits() - getCacheIndexBits();
 		return bits;
 	}	
 	
 	private int getTag(int address){
 		//int bits = getTagBits();
 		String addressString = Integer.toString(address);
-		String result = addressString.substring(0,31-getOffsetBits()-getSetIndexBits());
+		String result = addressString.substring(0,31-getOffsetBits()-getCacheIndexBits());
 		return Integer.parseInt(result);
 	}
 	
