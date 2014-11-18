4a80b52a7d4741cae5acfb4a08ab21b643a13e04
Swetha <gswetha@hotmail.com>
2014-11-18 16:27:46 +0800

    Bus runCacheProtocol change
    do it by transaction type

--- src/Bus.java
+++ src/Bus.java
@@ -43,36 +43,55 @@ public class Bus {
     	Cache target_cache = caches.get(cache_id);
     	CacheLine block = target_cache.getCacheBlock(br.getAddress());
     	
-        if (protocol == "MSI") {
+        switch(br.getTransaction()){
+	        case BusRd:
+	        	//get state of block that made this trxn
+	        	//if state was M, go to 
+	        	//if state was E, remain in E. 
+	        	//if state was S, remain in S. other S dont do anything. other in M flush and goes to S
+	        	//if state was I, go to S, others in S don't do anything. other in M flush and goes to S
+	        	
+	        	if (protocol == "MSI") {
 
-            switch (block.getState()) {
-                case MODIFIED:
-                    break;
-                case SHARED:
-                    break;
-                case INVALID:
-                    break;
-                default:
-                    break;
-            }
-            return;
-        }
+	                switch (block.getState()) {
+	                    case MODIFIED:
+	                        break;
+	                    case SHARED:
+	                        break;
+	                    case INVALID:
+	                        break;
+	                    default:
+	                        break;
+	                }
+	                return;
+	            }
 
-        if (protocol == "MESI") {
+	            if (protocol == "MESI") {
 
-            switch (block.getState()) {
-                case MODIFIED:
-                    break;
-                case EXCLUSIVE:
-                    break;
-                case SHARED:
-                    break;
-                case INVALID:
-                    break;
-                default:
-                    break;
-            }
-            return;
+	                switch (block.getState()) {
+	                    case MODIFIED:
+	                        break;
+	                    case EXCLUSIVE:
+	                        break;
+	                    case SHARED:
+	                        break;
+	                    case INVALID:
+	                        break;
+	                    default:
+	                        break;
+	                }
+	                return;
+	            }
+	        	break;
+	        case BusRdX:
+	        	//get state of block that made this trxn
+	        	//if state was M, no need to to anything cos the others are already I and you remain in M
+	        	//if state was E, go to M, rest go to I
+	        	//if state was S, go to M, rest go to I
+	        	//if state was I, go to M, rest go to I
+	        	break;
+	    	default:
+	    		break;
         }
     }
     
